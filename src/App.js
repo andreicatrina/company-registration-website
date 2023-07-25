2b30e0f8400006e253c5534d905c27da52737aa8
Andrei <catrina.andrei93@yahoo.com>
2023-07-25 22:41:18 +0300

    Project initialised

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { LandingSection } from "./components/LandingSection/LandingSection";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <LandingSection />
+    </>
+  );
+}
+
+export default App;
